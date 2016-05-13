027f1a60929e3d7ed4a6c1c31bd29d139f5ed8ba
Oliver Dolgener <o.dolgener@googlemail.com>
2016-05-13 08:32:16 +0200

    Added Initial Project

---
+++ BGM/Assets/AudioTrigger.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+var victoryClip:AudioClip;
+var soundSrc:AudioSource;
+var BGM:GameObject;
+var bgmSrc:AudioSource;
+
+function Start() {
+	soundSrc = GetComponent.<AudioSource>();
+	BGM = GameObject.Find("BGM");
+	bgmSrc = BGM.GetComponent.<AudioSource>();
+}
+
+function OnTriggerEnter(o:Collider) {
+	soundSrc.PlayOneShot(victoryClip);
+	if (bgmSrc.isPlaying) {
+		bgmSrc.Pause();
+	}
+}
+
+function OnTriggerExit(o:Collider) {
+	if (!bgmSrc.isPlaying) {
+		bgmSrc.Play();
+	}
+}
